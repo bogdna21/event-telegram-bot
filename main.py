5d9430334a9131ef6a072dbd774e8575b03162a3
bc2024-4 <bzabrodskij20@gmail.com>
2025-08-11 21:13:55 +0300

    fix

--- main.py
+++ main.py
@@ -826,8 +826,8 @@ def export_event_handler(message):
 
 # === Вебхуки ===
 
-@app.route(f"/bot/{API_TOKEN}", methods=['POST'])
-def webhook():
+@app.route('/', methods=['POST'])
+def webhook_root():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
